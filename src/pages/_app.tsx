b7fb8a85889fcf2f592b5ac92af84b1e4307deaf
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2022-09-22 11:09:50 -0300

    chore: config eslint + prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+
 import { globalStyles } from '../styles/global'
 
 globalStyles()
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,15 +1,6 @@
 import { createStitches } from '@stitches/react'
 
-export const {
-  config,
-  styled,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-  createTheme
-} = createStitches({
+export const { config, styled, css, globalCss, keyframes, getCssText, theme, createTheme } = createStitches({
   theme: {
     colors: {
       white: '#FFF',
