97e69db75b04f9059c7f6ce2f1f4f18de636a1d6
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2022-09-24 20:42:41 -0300

    feat: :sparkles: add app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app'
 
+import Logo from '~/assets/logo'
 import { globalStyles } from '~/styles/global'
+import { Container, Header } from '~/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Logo />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
