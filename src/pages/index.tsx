8681e5df04c3bc89e30a24adc7b990ebae4947bc
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2022-09-24 23:53:50 -0300

    perf: :zap: create static home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import 'keen-slider/keen-slider.min.css'
 
 import { useKeenSlider } from 'keen-slider/react'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Image from 'next/future/image'
 import Stripe from 'stripe'
 
@@ -43,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const { data } = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -61,6 +61,7 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
